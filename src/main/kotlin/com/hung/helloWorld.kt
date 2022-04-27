35df3beeffbc0472a1b230b85cf0f36380a2d262
NQHung <53897463+b1704735@users.noreply.github.com>
2022-04-27 16:39:35 +0700

    Update helloWorld.kt
    Response "hello $name" with name from path

--- src/main/kotlin/com/hung/helloWorld.kt
+++ src/main/kotlin/com/hung/helloWorld.kt
@@ -2,15 +2,19 @@ package com.example
 
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
+import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 fun HelloWorld(): HttpHandler {
-    return routes("/" bind GET to { Response(OK).body("hello http4k")})
+    return routes("/hello" bind GET to { request: Request ->
+        val name = request.query("name") ?: error("Name is required")
+        Response(OK).body("hello $name")})
 }
 
 fun main() {
